cdb37a42c5ed340d3efcf1fb2d4eb5008c715412
Valentintesic2 <valentin.tesic45@gmail.com>
2025-03-15 14:49:00 +0100

    Forum_backend_vorerst_fertig

--- website/forum.py
+++ website/forum.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from .models import Post, Comment
+from . import db
 
 
 forum = Blueprint('forum', __name__)
